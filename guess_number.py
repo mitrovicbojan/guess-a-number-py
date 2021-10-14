18fe30aa18817a473095d043f7406d5abc96f056
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2021-10-14 09:19:36 +0300

    first commit

---
+++ guess_number.py
@@ -0,0 +1,36 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    if top_of_range <= 0:
+        print("Enter a number larger than 0.")
+        quit()
+else: 
+    print("Enter a number next time.")
+    quit()
+
+random_num = random.randint(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Type a number next time.")
+        quit()
+        continue
+
+    if user_guess == random_num:
+        print("You got it!")
+        break
+    elif user_guess > random_num:
+            print("you were above the number")
+    else:
+        print("you were below the number")
+
+print("you got it in", guesses, "guesses")
+    
